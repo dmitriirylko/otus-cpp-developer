a66cc955741a01d7ab3beacd866996abb6a92ced
dmitriirylko <drv01101998@gmail.com>
2024-07-15 00:30:07 +0300

    Packaging and tests added

---
+++ hw1-build-test-deploy/CMakeFiles/helloworld_cli.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld_cli.dir/lib.cpp.o"
+  "CMakeFiles/helloworld_cli.dir/lib.cpp.o.d"
+  "CMakeFiles/helloworld_cli.dir/main.cpp.o"
+  "CMakeFiles/helloworld_cli.dir/main.cpp.o.d"
+  "helloworld_cli"
+  "helloworld_cli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

d232e01d498cb31593d924c76d9fb078081f37b5
dmitriirylko <drv01101998@gmail.com>
2024-07-21 12:09:41 +0300

    Version log added to main

--- hw1-build-test-deploy/main.cpp
+++ hw1-build-test-deploy/main.cpp
@@ -5,6 +5,11 @@
 int main()
 {
     std::cout << "Version: " << version() << std::endl;
+    auto closure = []()
+    {
+        return isVersionGreater(version(), "0.0.0") ? std::string("true") : std::string("false");
+    };
+    std::cout << "Is version greater than 0.0.0: " << closure() << std::endl;
     std::cout << "Hello, world!" << std::endl;
     
     return 0;
--- hw1-build-test-deploy/test_version.cpp
+++ hw1-build-test-deploy/test_version.cpp
@@ -6,58 +6,11 @@
 
 #include "lib.h"
 
-/**
- * @brief Compares two versions represented as strings in format
- * 			"n1.n2.n3" like (1.12.52).
- *  @param v1 Version (string) to check.
- *  @param v2 Version (string) to check with.
- *  @return true if v1 > v2, false otherwise.
- */
-bool isVersionGreater(std::string v1, std::string v2)
-{
-	/* Stores integer value of each part of version. */
-    int vnum1 = 0, vnum2 = 0;
-
-    for(int i = 0, j = 0; (i < v1.length() || j < v2.length());)
-	{
-		/* Convert one part of version to number. */
-        while(i < v1.length() && v1[i] != '.')
-		{
-            vnum1 = vnum1 * 10 + (v1[i] - '0');
-            i++;
-        }
-		/* Convert other part of version to number. */
-        while(j < v2.length() && v2[j] != '.')
-		{
-            vnum2 = vnum2 * 10 + (v2[j] - '0');
-            j++;
-        }
-        /* Compare part of versions. */
-        if(vnum1 > vnum2)
-		{
-			return true;
-		}
-        else if(vnum1 < vnum2)
-		{
-			return false;
-		}
-		else
-		{
-            /* Reset number variables and go for next numeric part. */ 
-            vnum1 = vnum2 = 0;
-            i++;
-            j++;
-            continue;
-        }
-    }
-    return false;
-}
-
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-	BOOST_CHECK(isVersionGreater("13.2.4.1", "0.0.0"));
+	BOOST_CHECK(isVersionGreater(version(), "0.0.0"));
 }
 
 BOOST_AUTO_TEST_SUITE_END()

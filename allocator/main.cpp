7cb98f098a4fdf51c47fbe01cc9000160a23c7e5
dmitriirylko <drv01101998@gmail.com>
2024-08-29 06:55:36 +0300

    allocator updated

--- allocator/main.cpp
+++ allocator/main.cpp
@@ -4,36 +4,37 @@
 
 #include "allocator.h"
 #include "myclass.h"
-#include "poolallocator.h"
+#include "poolalloc.h"
 #include "linearallocator.h"
 #include "forwardlist.h"
 
 int main()
 {
-    // std::list<int, PoolAllocator<int, 3>> mylist;
-    // mylist.emplace_back(52);
+    std::list<int, PoolAllocator<int, 7>> mylist;
+    mylist.emplace_back(52);
     // mylist.pop_back();
-    // mylist.emplace_back(25);
-    // std::list<int, PoolAllocator<int, 3>> mylist1;
-    // mylist1 = std::move(mylist);
-    // mylist1.emplace_back(255);
+    mylist.emplace_back(25);
+    mylist.emplace_back(25);
+    std::list<int, PoolAllocator<int, 7>> mylist1{mylist};
+    // mylist1 = mylist;
+    mylist1.emplace_back(255);
     // mylist1.emplace_back(44);
     // mylist1.emplace_back(43);
-    // std::cout << mylist1.back() << ", at: " << &mylist1.back() << std::endl;
+    std::cout << mylist1.back() << ", at: " << &mylist1.back() << std::endl;
     ////////////////////////////////////////////////////////////////////////////
     // std::vector<int, PoolAllocator<int>> myvector;
     // myvector.push_back(52);
     // myvector.push_back(52);
     //////////////////////////////////////////////////////////////////////
-    ForwardList<int, PoolAllocator<int, 3>> customList;
-    customList.push_back(25);
-    customList.push_back(52);
-    customList.push_back(522);
-    auto it = customList.find(52);
-    customList.insert_after(it, 53);
-    for(int elem : customList)
-    {
-        std::cout << elem << std::endl;
-    }
+    // ForwardList<int, PoolAllocator<int, 3>> customList;
+    // customList.push_back(25);
+    // customList.push_back(52);
+    // customList.push_back(522);
+    // auto it = customList.find(52);
+    // customList.insert_after(it, 53);
+    // for(int elem : customList)
+    // {
+    //     std::cout << elem << std::endl;
+    // }
     return 0;
 }
--- allocator/poolalloc.h
+++ allocator/poolalloc.h
@@ -1,115 +1,167 @@
 #pragma once
 
 #include <stack>
+#include <queue>
 #include <memory>
-#include <span>
 
-class pool
+// #define LOGGING
+
+class Pool
 {
 public:
-    using size_type = std::size_t;
-    explicit pool(size_type size) : m_size{size}
-    {}
+    /**
+     * @brief Pool constructor.
+     * @param typeSize Size of data structure which is going to be allocated in pool.
+     * @param elementsNum Number of elements pool have to store in one memory block.
+     */
+    explicit Pool(std::size_t typeSize, std::size_t elementsNum = 10) :
+        m_typeSize{typeSize},
+        m_elementsNum{elementsNum}
+    {
+        addAddrs();
+    }
+    /**
+     * @brief Gets pointer out of the pool for further allocation. If there are no
+     *          vacant addresses in pool it tries to allocate new memory block.
+     * @return Pointer out of the pool for allocation.
+     */
     void* allocate()
     {
-        if(m_addrs.empty())
+        uint8_t* currentPtrCopy = m_currentPtr;
+        m_currentPtr += m_typeSize;
+        if(m_currentPtr > m_finalPtr)
         {
-            add_addrs();
+            throw std::bad_alloc();
         }
-        auto ptr = m_addrs.top();
-        m_addrs.pop();
-        std::cout << ptr << std::endl;
-        return ptr;
+        #ifdef LOGGING
+        std::cout << (void*)currentPtrCopy << std::endl;
+        #endif
+        return currentPtrCopy;
     }
+    /**
+     * @brief Returns deallocated pointer to addresses queue.
+     */
     void deallocate(void *ptr)
+    {}
+    /**
+     * @brief Changes size of data structure which is going to be allocated in pool.
+     *          Rebinding after any allocation is unsupported.
+     * @param Size of data structure which is going to be allocated in pool.
+     */
+    void rebind(std::size_t typeSize)
     {
-        m_addrs.push(ptr);
+        m_typeSize = typeSize;
     }
-    void rebind(size_type size)
+    void addAddrs()
     {
-        if(!m_addrs.empty() || !m_blocks.empty())
-        {
-            std::cerr << "rebind after alloc is unsupported" << std::endl;
-            abort();
-        }
-        m_size = size;
-    }
-    void add_addrs()
-    {
-        constexpr const auto block_size = 0x1000;
-        auto block = std::make_unique<uint8_t[]>(block_size);
-        auto v = std::span<uint8_t>(block.get(), block_size);
-        auto total_size = v.size() % m_size == 0 ? v.size() : v.size() - m_size;
-        for(auto i = 0; i < total_size; i += m_size)
-        {
-            m_addrs.push(&v[i]);
-        }
-        m_blocks.push(std::move(block));
+        m_block = std::make_unique<uint8_t[]>(m_typeSize * m_elementsNum);
+        m_currentPtr = m_block.get();
+        m_finalPtr = m_block.get() + m_elementsNum * m_typeSize;
+        #ifdef LOGGING
+        std::cout << " add_addrs ";
+        #endif
     }
 
 private:
-    size_type m_size;
-    std::stack<void*> m_addrs{};
-    std::stack<std::unique_ptr<uint8_t[]>> m_blocks{};
+    /**
+     * @brief Size of data structure which is going to be allocated in pool.
+     */
+    std::size_t m_typeSize;
+    /**
+     * @brief Number of elements pool have to store in one memory block.
+     */
+    size_t m_elementsNum;
+    /**
+     * @brief Queue of vacant addresses which can be used in allocation.
+     */
+    // std::queue<void*> m_addrs{};
+    /**
+     * @brief Stack of memory blocks. Block are added when previous
+     *          one has been run out or there are no blocks in stack.
+     */
+    // std::stack<std::unique_ptr<uint8_t[]>> m_blocks{};
+    std::unique_ptr<uint8_t[]> m_block;
+    uint8_t* m_currentPtr = nullptr;
+    uint8_t* m_finalPtr = nullptr;
 };
 
-template<typename T>
-class poolallocator
+template<typename T, size_t elementsNum = 10>
+class PoolAllocator
 {
 public:
     using value_type = T;
     using pointer = T*;
     using size_type = std::size_t;
     using is_always_equal = std::false_type;
-    using propagate_on_container_copy_assignment = std::false_type;
+    using propagate_on_container_copy_assignment = std::true_type;
     using propagate_on_container_move_assignment = std::true_type;
     using propagate_on_container_swap = std::true_type;
     template <typename U>
-    struct rebind {
-        using other = poolallocator<U>;
-    };
-    poolallocator() : m_pool{std::make_shared<pool>(sizeof(T))}
+    struct rebind
     {
+        using other = PoolAllocator<U, elementsNum>;
+    };
+    PoolAllocator() : m_pool{std::make_shared<Pool>(sizeof(T), elementsNum)}
+    {
+        #ifdef LOGGING
         std::cout << this << " constructor, sizeof(T): " << sizeof(T) << '\n';
+        #endif
     }
     template<typename U>
-    poolallocator(const poolallocator<U>& other) noexcept : m_pool{other.m_pool}
+    PoolAllocator(const PoolAllocator<U, elementsNum>& other) noexcept : m_pool{other.m_pool}
     {
+        #ifdef LOGGING
         std::cout << this << " copy constructor (U), sizeof(T): " << sizeof(T) << '\n';
+        #endif
         m_pool->rebind(sizeof(T));
     }
-    poolallocator(poolallocator&& other) noexcept : m_pool{std::move(other.m_pool)}
+    PoolAllocator(PoolAllocator&& other) noexcept : m_pool{std::move(other.m_pool)}
     {
+        #ifdef LOGGING
         std::cout << this << " move constructor, sizeof(T): " << sizeof(T) << '\n';
+        #endif
     }
-    poolallocator& operator=(poolallocator&& other) noexcept
+    PoolAllocator& operator=(PoolAllocator&& other) noexcept
     {
-        std::cout << this << " move assignment, sizeof(T): " << sizeof(T) << '\n';
+
         m_pool = std::move(other.m_pool);
+        #ifdef LOGGING
+        std::cout << this << " move assignment, sizeof(T): " << sizeof(T) << '\n';
+        #endif
         return *this;
     }
-    poolallocator(const poolallocator& other) noexcept : m_pool{other.m_pool}
+    PoolAllocator(const PoolAllocator& other) noexcept : m_pool{other.m_pool}
     {
+        #ifdef LOGGING
         std::cout << this << " copy constructor, sizeof(T): " << sizeof(T) << '\n';
+        #endif
     }
-    poolallocator& operator=(const poolallocator& other) noexcept
+    PoolAllocator& operator=(const PoolAllocator& other) noexcept
     {
-        std::cout << this << " copy assignment, sizeof(T): " << sizeof(T) << '\n';
         m_pool = other.m_pool;
+        #ifdef LOGGING
+        std::cout << this << " copy assignment, sizeof(T): " << sizeof(T) << '\n';
+        #endif
         return *this;
     }
     pointer allocate(size_type n)
     {
+        #ifdef LOGGING
         std::cout << this << " A [" << n << "]: ";
+        #endif
+        /* In case of allocation of more than one element our custom allocator works as std::allocator. */
         if(n != 1)
         {
             return static_cast<pointer>(malloc(sizeof(T) * n));
         }
         return static_cast<pointer>(m_pool->allocate());
     }
+    /* In case of deallocation of more than one element our custom allocator works as std::allocator. */
     void deallocate(pointer ptr, size_type n)
     {
+        #ifdef LOGGING
         std::cout << this << " D [" << n << "]: " << ptr << std::endl;
+        #endif
         if(n != 1)
         {
             free(ptr);
@@ -119,33 +171,32 @@ public:
     template <typename U, typename... Args>
     void construct(U *p, Args &&...args)
     {
+        #ifdef LOGGING
         std::cout << this << " C [" << 1 << "]: " << p << std::endl;
+        #endif
         new (p) U(std::forward<Args>(args)...);
     };
 
     template <typename U>
-    void destroy(U *p) {
+    void destroy(U *p)
+    {
+        #ifdef LOGGING
         std::cout << this << " ~ [" << 1 << "]: " << p << std::endl;
+        #endif
         p->~U();
     }
+
+    bool operator==(const PoolAllocator& other)
+    {
+        return m_pool.get() == other.m_pool.get();
+    }
+
+    bool operator!=(const PoolAllocator& other)
+    {
+        return m_pool.get() != other.m_pool.get();
+    }
 private:
-    std::shared_ptr<pool> m_pool;
-    template<typename T1, typename T2>
-    friend bool operator==(const poolallocator<T1> &lhs, const poolallocator<T2> &rhs);
-    template<typename T1, typename T2>
-    friend bool operator!=(const poolallocator<T1> &lhs, const poolallocator<T2> &rhs);
-    template <typename U>
-    friend class poolallocator;
+    std::shared_ptr<Pool> m_pool;
+    template <typename U, size_t>
+    friend class PoolAllocator;
 };
-
-template<typename T1, typename T2>
-bool operator==(const poolallocator<T1> &lhs, const poolallocator<T2> &rhs)
-{
-    return lhs.m_pool.get() == rhs.m_pool.get();
-}
-
-template<typename T1, typename T2>
-bool operator!=(const poolallocator<T1> &lhs, const poolallocator<T2> &rhs)
-{
-    return lhs.m_pool.get() != rhs.m_pool.get();
-}

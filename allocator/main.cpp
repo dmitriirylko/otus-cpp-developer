fcf7e2f7d7cd6b18e36cae1916f98359be5fbb07
dmitriirylko <drv01101998@gmail.com>
2024-08-29 22:27:27 +0300

    hw3 finished

--- allocator/main.cpp
+++ allocator/main.cpp
@@ -10,13 +10,13 @@
 
 int main()
 {
-    // std::list<int, PoolAllocator<int, 7>> mylist;
-    // mylist.emplace_back(52);
-    // mylist.emplace_back(25);
-    // mylist.emplace_back(25);
-    // std::list<int, PoolAllocator<int, 7>> mylist1{mylist};
-    // mylist1.emplace_back(255);
-    // std::cout << mylist1.back() << ", at: " << &mylist1.back() << std::endl;
+    std::list<int, PoolAllocator<int, 3>> mylist;
+    mylist.emplace_back(52);
+    mylist.emplace_back(25);
+    mylist.emplace_back(25);
+    std::list<int, PoolAllocator<int, 3>> mylist1{mylist};
+    mylist1.emplace_back(255);
+    std::cout << mylist1.back() << ", at: " << &mylist1.back() << std::endl;
 
     ////////////////////////////////////////////////////////////////////////////
     // std::vector<int, PoolAllocator<int>> myvector;
@@ -24,16 +24,16 @@ int main()
     // myvector.push_back(52);
 
     //////////////////////////////////////////////////////////////////////
-    ForwardList<int, PoolAllocator<int, 30>> customList;
-    customList.pushBack(25);
-    customList.pushBack(52);
-    customList.pushBack(522);
-    auto it = customList.find(52);
-    customList.insertAfter(it, 53);
-    for(int elem : customList)
-    {
-        std::cout << elem << std::endl;
-    }
+    // ForwardList<int, PoolAllocator<int, 30>> customList;
+    // customList.pushBack(25);
+    // customList.pushBack(52);
+    // customList.pushBack(522);
+    // auto it = customList.find(52);
+    // customList.insertAfter(it, 53);
+    // for(int elem : customList)
+    // {
+    //     std::cout << elem << std::endl;
+    // }
 
     /////////////////////////////////////////////////////////////////
     // std::vector<int, PoolAllocator<int, 100>> vec;
--- hw3-allocator/main.cpp
+++ hw3-allocator/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <map>
 
-#include "linearallocator.h"
+#include "poolallocator.h"
+#include "forwardlist.h"
 
 int factorial(int n)
 {
@@ -13,125 +13,36 @@ int factorial(int n)
     return n * factorial(n - 1);
 }
 
-template<typename T>
-struct SimpleAlloc
-{
-    using value_type = T;
-    SimpleAlloc() = default;
-    T* allocate(size_t n)
-    {
-        return reinterpret_cast<T*>(new char[n * sizeof(T)]);
-    }
-    void deallocate(T* p, size_t n)
-    {
-        delete [] reinterpret_cast<char*>(p);
-    }
-};
-
-class Arena
-{
-public:
-    /* @brief Allocate from pp[0, ss-1]. */
-    Arena(char* pp, int ss) : p(pp), s(ss)
-    {}
-    ~Arena()
-    {
-        delete [] p;
-    }
-    char* p;
-    int s;
-};
-
-template<typename T>
-struct MyAlloc
-{
-    using value_type = T;
-    MyAlloc(Arena &aa) : a(aa)
-    {
-        ptr = a.p;
-    }
-    MyAlloc() = delete;
-    T* allocate(size_t n)
-    {
-        ptr+= n * sizeof(T);
-        std::cout << ptr << std::endl;
-        return reinterpret_cast<T*>(ptr);
-    }
-    void deallocate(T* p, size_t n)
-    {
-        ptr-= n * sizeof(T);
-    }
-    Arena &a;
-    char* ptr;
-};
-
-template<typename T>
-struct LoggingAlloctor
-{
-    using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
-    LoggingAlloctor() = default;
-    ~LoggingAlloctor() = default;
-    T* allocate(std::size_t n)
-    {
-        std::cout << "allocate: [n = " << n << "]" << std::endl;
-        auto p = std::malloc(n * sizeof(T));
-        if(!p)
-        {
-            throw std::bad_alloc();
-        }
-        return reinterpret_cast<T*>(p);
-    }
-    void deallocate(T *p, std::size_t n)
-    {
-        std::cout << "deallocate: [n = " << n << "]" << std::endl;
-        std::free(p);
-    }
-};
-
 int main()
 {
-    try
+    constexpr int poolSize = 10;
+    std::map<int, int> map;
+    for(int i = 0; i < poolSize; ++i)
     {
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // std::vector<int, LinearAlloctor<int>> vec{LinearAlloctor<int>(std::size_t(10))};
-        // std::cout << "Initial cap: " << vec.capacity() << std::endl;
-        // vec.push_back(11);
-        // std::cout << "Cap: " << vec.capacity() << std::endl;
-        // vec.push_back(22);
-        // std::cout << "Cap: " << vec.capacity() << std::endl;
-        // vec.push_back(33);
-        // std::cout << "Cap: " << vec.capacity() << std::endl;
-        // vec.push_back(44);
-        // std::cout << "Cap: " << vec.capacity() << std::endl;
-        // // vec.push_back(55);
-        // // std::cout << "Cap: " << vec.capacity() << std::endl;
-        // std::cout << "Vec values:" << std::endl;
-        // for(int value : vec)
-        // {
-        //     std::cout << value << "\t";
-        // }
-        // std::cout << std::endl;
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////
-        std::map<int, int> map;
-        for(int i = 0; i < 10; ++i)
-        {
-            map[i] = factorial(i);
-        }
-        std::map<int, int, std::less<int>, LinearAlloctor<std::pair<const int, int>, 10>> map1{};
-        for(int i = 0; i < 11; ++i)
-        {
-            map1.emplace(i, factorial(i));
-        }
-        int a = 1;
+        map.emplace(i, factorial(i));
     }
-    catch(const std::exception& e)
+    std::map<int, int, std::less<int>, PoolAllocator<std::pair<const int, int>, poolSize>> mapAlloc;
+    for(int i = 0; i < poolSize; ++i)
     {
-        std::cerr << e.what() << std::endl;
+        mapAlloc.emplace(i, factorial(i));
+    }
+    for(const auto& pair : mapAlloc)
+    {
+        std::cout << pair.first << " " << pair.second << std::endl;
+    }
+    ForwardList<int> list;
+    for(int i = 0; i < poolSize; ++i)
+    {
+        list.pushBack(i);
+    }
+    ForwardList<int, PoolAllocator<int, poolSize>> listAlloc;
+    for(int i = 0; i < poolSize; ++i)
+    {
+        listAlloc.pushBack(i);
+    }
+    for(const auto& elem : listAlloc)
+    {
+        std::cout << elem << std::endl;
     }
     return 0;
 }

5216f159dfbdca89b03cb4ac633ae65c69a7bff4
dmitriirylko <drv01101998@gmail.com>
2024-09-19 06:58:14 +0300

    proxy moved to separate class

--- hw6-matrix/main.cpp
+++ hw6-matrix/main.cpp
@@ -3,9 +3,9 @@
 int main()
 {
     Matrix<int, -1> matrix;
-    std::cout << matrix.size() << std::endl;
+    // std::cout << matrix.size() << std::endl;
     matrix[100][100] = 314;
-    std::cout << matrix[100][100] << std::endl;
-    std::cout << matrix.size() << std::endl;
+    std::cout << matrix[100][101] << std::endl;
+    // std::cout << matrix.size() << std::endl;
     return 0;
 }
--- hw6-matrix/matrix.h
+++ hw6-matrix/matrix.h
@@ -4,40 +4,19 @@
 #include <map>
 #include <utility>
 
+template<typename T, T defaultValue> class MatrixProxy;
+
 template<typename T, T defaultValue>
 class Matrix
 {
-private:
-    class MatrixProxy
-    {
-    public:
-        MatrixProxy(Matrix* matrix, size_t row) :
-            m_ptr(matrix),
-            m_row(row)
-        {}
-
-        T& operator[](size_t col)
-        {
-            return m_ptr->m_data[std::make_pair(m_row, col)];
-        } 
-
-    private:
-        size_t m_row;
-        Matrix* m_ptr;
-    };
-
 public:
     Matrix() = default;
 
     ~Matrix() = default;
+
     size_t size()
     {
         return m_data.size();
-    }
-
-    void add(size_t row, size_t col, T val)
-    {
-        m_data[std::make_pair(row, col)] = val;
     }
 
     void print()
@@ -48,11 +27,38 @@ public:
         }
     }
 
-    MatrixProxy operator[](size_t row)
+    MatrixProxy<T, defaultValue> operator[](size_t row)
     {
-        return MatrixProxy(this, row);
+        return MatrixProxy(*this, row);
     }
 
 private:
     std::map<std::pair<size_t, size_t>, T> m_data;
+    friend MatrixProxy<T, defaultValue>;
+};
+
+template<typename T, T defaultValue>
+class MatrixProxy
+{
+public:
+    MatrixProxy(Matrix<T, defaultValue>& matrix, size_t row) :
+        m_matrix{matrix},
+        m_row{row}
+    {}
+
+    ~MatrixProxy() = default;
+
+    T& operator[](size_t col)
+    {
+        auto key = std::make_pair(m_row, col);
+        if(m_matrix.m_data.find(key) == m_matrix.m_data.end())
+        {
+            return;
+        }
+        return m_matrix.m_data[key];
+    } 
+
+private:
+    size_t m_row;
+    Matrix<T, defaultValue>& m_matrix;
 };

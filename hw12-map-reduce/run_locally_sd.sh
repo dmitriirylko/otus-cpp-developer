5dc91ceb58147712087337a74121cd2097caa207
dmitriirylko <drv01101998@gmail.com>
2024-12-07 15:44:10 +0300

    sd renamed to var

--- hw12-map-reduce/run_locally_sd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-BINARY_HOME=./bin
-INPUT_HOME=./input
-INPUT=${INPUT_HOME}/AB_NYC_2019.csv
-
-cat ${INPUT} | ${BINARY_HOME}/mapper_sd "$(cat output_mean)" | ${BINARY_HOME}/reducer_sd > output_sd
-cat output_sd
